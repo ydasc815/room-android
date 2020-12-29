cd8c50736f0dd934b099866a7f5e1b2514d61b60
Aditya <icgadi99@gmail.com>
2020-12-29 14:52:41 +0530

    --update: exception handling on db operations

--- app/src/main/java/com/aditya/room_android/MainActivity.kt
+++ app/src/main/java/com/aditya/room_android/MainActivity.kt
@@ -2,6 +2,8 @@ package com.aditya.room_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.room.Room
 import com.aditya.room_android.database.User
@@ -9,9 +11,11 @@ import com.aditya.room_android.database.UserDao
 import com.aditya.room_android.database.UserDatabase
 import com.aditya.room_android.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private val TAG: String = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -32,8 +36,18 @@ class MainActivity : AppCompatActivity() {
 
     private suspend fun databaseOperations(users: UserDao, name: String, age: String, gender: String) {
         Dispatchers.IO {
-            users.insert(User(null, name, age.toInt(), gender))
-            println("SQL Test : ${users.getAllUsers()}")
+            try {
+                users.insert(User(null, name, age.toInt(), gender))
+                runOnUiThread {
+                    Toast.makeText(applicationContext, "Data added successfully", Toast.LENGTH_LONG).show()
+                }
+                println("SQL Test : ${users.getAllUsers()}")
+            } catch (e: Exception) {
+                Log.d(TAG, "DatabaseOperationException: $e")
+                runOnUiThread {
+                    Toast.makeText(applicationContext, "Could not add data to database", Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
